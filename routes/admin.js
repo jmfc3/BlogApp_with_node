6c54f555b2f4feba11a0d7adbe0d4def030e2766
Miguel Cardoso <a39233@alunos.ipb.pt>
2020-08-12 14:55:03 +0100

    Alterando dados na base de dados mongoDB

--- routes/admin.js
+++ routes/admin.js
@@ -12,9 +12,16 @@ router.get('/posts', (req, res) => {
     res.send('Pagina de posts')
 })
 
-router.get('/categorias', (req, res) => {
-    res.render('admin/categorias')
-})
+
+router.get('/categorias', async (req, res, next) => {
+    try {
+      const categorias = await Categoria.find();
+      return res.render("admin/categorias", {categorias: categorias.map(categorias => categorias.toJSON())})
+    } catch (err) {
+      req.flash("error_msg", "Houve um erro ao carregar as categorias")
+      res.redirect('/')
+    }
+  })
 
 router.get('/categorias/add', (req, res) => {
     res.render('admin/addcategoria')
@@ -53,6 +60,38 @@ router.post('/categorias/nova', (req, res) => {
     }
 } )
 
+
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria)=>{
+        res.render('admin/editcategorias', {categoria:categoria})
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria nao existe")
+        res.redirect("/admin/categorias")
+    })
+    
+})
+
+router.post("/categorias/edit", (req, res) => {
+
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+    
+        categoria.save().then( () => {
+            req.flash("success_msg", "Categoria editada com sucesso")
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao salvar a categoria")
+            res.redirect('/admin/categorias')
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar a categoria")
+        res.redirect('/admin/categorias')
+    })
+
+})
 
 
 module.exports = router
